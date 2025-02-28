19e270391466dd23e8402f5794d5d11cf66448a3
devcube2 <jejeje8@knou.ac.kr>
2025-02-28 15:31:09 +0900

    01장 - 어노테이션으로 DI 사용하기

---
+++ Ex03_AnnotationDI/src/main/java/com/study/springboot/bean/Ex03JavaCodeDiApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot.bean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex03JavaCodeDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex03JavaCodeDiApplication.class);
+	}
+
+}
